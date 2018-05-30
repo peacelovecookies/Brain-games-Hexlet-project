ae410f193619af453bfef1748583932f909e2e9e
Pavel Kim <kimpav95@mail.ru>
2018-05-30 14:53:15 +0500

    edit .js following eslint recommendations

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
 import { sayHi } from '..';
-console.log("Welcome to the Brain Games!");
+
+console.log('Welcome to the Brain Games!');
 sayHi();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const sayHi = () => {
-    const userName = readlineSync.question('May I have your name, please? ');
-    console.log(`Hi, my dear, ${userName}`);
+const sayHi = () => {
+  const userName = readlineSync.question('May I have your name, please? ');
+  console.log(`Hi, my dear, ${userName}`);
 };
+
+export default sayHi;

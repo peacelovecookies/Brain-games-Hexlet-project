03cc3ede834f355a379fd1dc5cd1ab9a2e63bf1d
Pavel Kim <kimpav95@mail.ru>
2018-05-31 01:37:46 +0500

    add askIsEven method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import readlineSync from 'readline-sync';
 
-const sayHi = () => {
+export const sayHi = () => {
   const userName = readlineSync.question('May I have your name, please? ');
   console.log(`Hi, my dear, ${userName}`);
+  return userName;
 };
 
-export default sayHi;
+export const askIsEven = () => {
+  const userAnwer = readlineSync.question('Your answer: ');
+  return userAnwer;
+};

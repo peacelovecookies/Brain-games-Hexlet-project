f47f59b0ddd9a3d7806b54f4ef7c2332f80902e6
Pavel Kim <kimpav95@mail.ru>
2018-05-31 01:42:36 +0500

    add 'brain-even' game script

---
+++ src/bin/brain-even.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import { sayHi, askIsEven } from '..';
+
+console.log('Welcome to the Brain Games!', 'Answer "yes" if number even otherwise answer "no"');
+
+const userName = sayHi();
+
+const toPlay = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const num = Math.floor(Math.random() * (20 - 1)) + 1;
+    console.log(`Question: ${num}`);
+    const rightAnswer = num % 2 === 0 ? 'yes' : 'no';
+    const userAnwer = askIsEven();
+
+    if (rightAnswer !== userAnwer) {
+      console.log(`Sorry, but ${userAnwer} is wrong answer =( While right answer was ${rightAnswer}). Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}! You are a clever one ;)`);
+};
+
+toPlay();

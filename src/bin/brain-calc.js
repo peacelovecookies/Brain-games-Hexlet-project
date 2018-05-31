ebe0e45e997b6fd060902c8694cb5da62e7b21e6
Pavel Kim <kimpav95@mail.ru>
2018-05-31 08:32:47 +0500

    return 'toPlay' function, refactor it guided by the logic of the current game

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
 
-import { sayHi, randomNum, toPlay } from '..';
+import { sayHi, randomNum, askPlayer, checkAnswer } from '..';
 
 const task = 'What is the result of the expression?';
 const userName = sayHi(task);
 
-const num1 = randomNum(1, 40);
-const num2 = randomNum(1, 40);
-const question = `Result of the expression: ${num1} + ${num2}?`;
-const rightAnswer = String(num1 + num2);
-toPlay(userName, question, rightAnswer);
+const toPlay = (rounds) => {
+  for (let i = 0; i < rounds; i += 1) {
+    const num1 = randomNum(1, 40);
+    const num2 = randomNum(1, 40);
+    const operators = ['+', '-', '*'];
+    const expression = `${num1} ${operators[randomNum(0, 2)]} ${num2}`;
+
+    console.log(`Result of the expression: ${expression}?`);
+
+    const rightAnswer = eval(expression);
+    const userAnwer = askPlayer();
+    if (!checkAnswer(rightAnswer, userAnwer, userName)) return;
+  }
+  console.log(`Congratulations, ${userName}! You are a clever one ;)`);
+};
+
+toPlay(5);
--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 
-import { sayHi, randomNum, toPlay } from '..';
+import { sayHi, randomNum, askPlayer, checkAnswer } from '..';
 
 const task = 'Answer "yes" if number even otherwise answer "no"';
 const userName = sayHi(task);
 
-const num = randomNum(1, 20);
-const question = `Is this number even: ${num}?`;
-const rightAnswer = num % 2 === 0 ? 'yes' : 'no';
-toPlay(userName, question, rightAnswer);
+const toPlay = (rounds) => {
+  for (let i = 0; i < rounds; i += 1) {
+    const num = randomNum(1, 20);
+    console.log(`Is this number even: ${num}?`);
+    const rightAnswer = num % 2 === 0 ? 'yes' : 'no';
+    const userAnwer = askPlayer();
+    if (!checkAnswer(rightAnswer, userAnwer, userName)) return;
+  }
+  console.log(`Congratulations, ${userName}! You are a clever one ;)`);
+};
+
+toPlay(3);

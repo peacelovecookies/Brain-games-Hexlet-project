c30bc15c73c3c0c24a06f33059db605725610528
Pavel Kim <kimpav95@mail.ru>
2018-05-31 05:38:05 +0500

    move 'toPlay' method to src/index.js, add new vars using 'toPlay' method

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,25 +1,11 @@
 #!/usr/bin/env node
 
-import { sayHi, askIsEven } from '..';
+import { sayHi, randomNum, toPlay } from '..';
 
-console.log('Welcome to the Brain Games!\n\nAnswer "yes" if number even otherwise answer "no"\n');
+const task = 'Answer "yes" if number even otherwise answer "no"';
+const userName = sayHi(task);
 
-const userName = sayHi();
-
-const toPlay = () => {
-  for (let i = 0; i < 3; i += 1) {
-    const num = Math.floor(Math.random() * (20 - 1)) + 1;
-    console.log(`Is this number even: ${num}?`);
-    const rightAnswer = num % 2 === 0 ? 'yes' : 'no';
-    const userAnwer = askIsEven();
-
-    if (rightAnswer !== userAnwer) {
-      console.log(`\nSorry, but ${userAnwer} is wrong answer =( While right answer was ${rightAnswer}). Let's try again, ${userName}!`);
-      return;
-    }
-    console.log('Correct!\n');
-  }
-  console.log(`Congratulations, ${userName}! You are a clever one ;)`);
-};
-
-toPlay();
+const num = randomNum(1, 20);
+const question = `Is this number even: ${num}?`;
+const rightAnswer = num % 2 === 0 ? 'yes' : 'no';
+toPlay(userName, question, rightAnswer);

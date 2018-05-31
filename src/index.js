a4285a5963d14379114ecace821a33fa6bbaff88
Pavel Kim <kimpav95@mail.ru>
2018-05-31 05:30:05 +0500

    refactor 'sayHi' method, rename 'askIsEven' to 'askPlayer'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import readlineSync from 'readline-sync';
 
-export const sayHi = () => {
+export const sayHi = (task) => {
+  console.log(`Welcome to the Brain Games!\n\n${task}\n`);
   const userName = readlineSync.question('May I have your name, please? ');
   console.log(`Hi, my dear, ${userName}\n`);
   return userName;
 };
 
-export const askIsEven = () => {
+export const askPlayer = () => {
   const userAnwer = readlineSync.question('Your answer: ');
   return userAnwer;
 };

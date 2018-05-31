07e69844468dc4edab8c3a2652ab52c47ef7e1ef
Pavel Kim <kimpav95@mail.ru>
2018-05-31 05:33:43 +0500

    add 'randomNum' & 'toPlay' methods

--- src/index.js
+++ src/index.js
@@ -11,3 +11,19 @@ export const askPlayer = () => {
   const userAnwer = readlineSync.question('Your answer: ');
   return userAnwer;
 };
+
+export const randomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+export const toPlay = (userName, question, rightAnswer) => {
+  for (let i = 0; i < 3; i += 1) {
+    console.log(question);
+    const userAnwer = askPlayer();
+
+    if (rightAnswer !== userAnwer) {
+      console.log(`\nSorry, but ${userAnwer} is wrong answer =( While right answer was ${rightAnswer}). Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!\n');
+  }
+  console.log(`Congratulations, ${userName}! You are a clever one ;)`);
+};

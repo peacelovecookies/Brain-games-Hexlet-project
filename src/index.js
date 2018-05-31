27003c181779a2c66716c6f6a52c4be605afe232
Pavel Kim <kimpav95@mail.ru>
2018-05-31 08:30:37 +0500

    remove 'toPlay' method, add 'checkAnswer' instead

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 export const sayHi = (task) => {
   console.log(`Welcome to the Brain Games!\n\n${task}\n`);
   const userName = readlineSync.question('May I have your name, please? ');
-  console.log(`Hi, my dear, ${userName}\n`);
+  console.log(`Hi, my dear, ${userName}\n\nNow let's play!`);
   return userName;
 };
 
@@ -14,16 +14,11 @@ export const askPlayer = () => {
 
 export const randomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-export const toPlay = (userName, question, rightAnswer) => {
-  for (let i = 0; i < 3; i += 1) {
-    console.log(question);
-    const userAnwer = askPlayer();
-
-    if (rightAnswer !== userAnwer) {
-      console.log(`\nSorry, but ${userAnwer} is wrong answer =( While right answer was ${rightAnswer}). Let's try again, ${userName}!`);
-      return;
-    }
-    console.log('Correct!\n');
+export const checkAnswer = (rightAnswer, userAnwer, userName) => {
+  if (rightAnswer !== userAnwer) {
+    console.log(`\nSorry, but ${userAnwer} is wrong answer =( While right answer was ${rightAnswer}). Let's try again, ${userName}!`);
+    return false;
   }
-  console.log(`Congratulations, ${userName}! You are a clever one ;)`);
+  console.log('Correct!\n');
+  return true;
 };

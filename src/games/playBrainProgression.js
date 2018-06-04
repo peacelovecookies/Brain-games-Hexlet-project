34e97444f80e57c8024f1a8358ef6b34b11272ac
Pavel Kim <kimpav95@mail.ru>
2018-06-04 14:19:05 +0500

    rewrite 'a, d, n' constants, make them clear for better understanding

--- src/games/playBrainProgression.js
+++ src/games/playBrainProgression.js
@@ -7,20 +7,20 @@ const task = 'Find missing number in all following progressions.';
 
 export default () => {
   const gameProperties = () => {
-    let a = randomNum(1, 20);
-    const d = randomNum(1, 10);
-    const n = 10;
+    let currentNum = randomNum(1, 20);
+    const adjunctionNum = randomNum(1, 10);
+    const numbersCount = 10;
     const missedNumPosition = randomNum(0, 9);
     const progression = [];
     let rightAnswer;
-    for (let i = 0; i < n; i += 1) {
+    for (let i = 0; i < numbersCount; i += 1) {
       if (i === missedNumPosition) {
         progression.push('...');
-        rightAnswer = String(a);
-        a += d;
+        rightAnswer = String(currentNum);
+        currentNum += adjunctionNum;
       }
-      progression.push(a);
-      a += d;
+      progression.push(currentNum);
+      currentNum += adjunctionNum;
     }
     const question = `Which number is missed: ${progression.join(' ')}?`;
 

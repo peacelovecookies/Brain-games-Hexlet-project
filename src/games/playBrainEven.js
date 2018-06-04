aeed94269f51a636320adcd39fe88116d3a6f10a
Pavel Kim <kimpav95@mail.ru>
2018-06-04 13:23:25 +0500

    fix 'isEven' function - should return boolean

--- src/games/playBrainEven.js
+++ src/games/playBrainEven.js
@@ -3,7 +3,7 @@
 import toPlay from '..';
 import randomNum from '../utils';
 
-const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = num => (num % 2 === 0);
 
 const task = 'Answer "yes" if number even otherwise answer "no"';
 
@@ -11,7 +11,7 @@ export default () => {
   const gameProperties = () => {
     const num = randomNum(1, 100);
     const question = `Is this number even: ${num}?`;
-    const rightAnswer = isEven(num);
+    const rightAnswer = isEven(num) ? 'yes' : 'no';
     return { question, rightAnswer };
   };
 

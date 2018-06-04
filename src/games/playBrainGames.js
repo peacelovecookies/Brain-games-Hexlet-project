0c75458c985ff1b70c7e7f40f035d67ec25b6209
Pavel Kim <kimpav95@mail.ru>
2018-06-04 13:32:14 +0500

    add 'brain-games' game script

---
+++ src/games/playBrainGames.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!\n');
+const userName = readlineSync.question('May I have your name, please? ');
+console.log(`Hi, my dear, ${userName}\n\nNow let's play!`);
